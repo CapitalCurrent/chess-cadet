e9d888615915fcc684e5df5d1006d8a8d505cc86
CapitalCurrent Developer <Ryan@CapitalCurrent.net>
2026-06-05 17:44:36 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * Bump CACHE when you change a STABLE-named asset (e.g. a regenerated piece
  * PNG) and want every client to drop the old copy. Hashed bundles don't need it.
  */
-const CACHE = 'chess-cadet-v2';
+const CACHE = 'chess-cadet-v3';
 
 const PRECACHE = [
   './',
